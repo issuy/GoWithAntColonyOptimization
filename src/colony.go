908913b55e9c8a49fcbb79037c57c2df84aa47e1
Akito Ishitani <akito.eld@gmail.com>
2013-08-09 16:36:26 +0900

    インデックス作成

---
+++ src/colony.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+     fmt.Println("Start Program.")
+     
+     //fmt.Println("Input root data.")
+
+     //fmt.Println("Do ACO")
+
+     //fmt.Println("Resutls")
+
+     fmt.Println("End Program.")
+     
+}
