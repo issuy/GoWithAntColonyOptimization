5edb8cf22f158d2618cb348b3a015697ad2323a3
Akito Ishitani <akito.eld@gmail.com>
2013-08-10 11:23:03 +0900

    TSPファイル読み込み実装

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ GoWithAntColonyOptimization
 ===========================
 
 Go言語でACO(Ant Colony Optimization)
+
+ベンチマークにTSPを解いてみる（att48.tsp）
+
+http://elib.zib.de/pub/Packages/mp-testdata/tsp/tsplib/tsp/index.html
--- src/colony.go
+++ src/colony.go
@@ -1,16 +1,52 @@
 package main
 
-import "fmt"
+import(
+    "fmt"
+    "os"
+    "bufio"
+    "strings"
+    "strconv"
+)
+
+type City struct {
+     num int
+     x   float64
+     y   float64
+}
 
 func main() {
      fmt.Println("Start Program.")
      
-     //fmt.Println("Input root data.")
-
-     //fmt.Println("Do ACO")
-
-     //fmt.Println("Resutls")
-
-     fmt.Println("End Program.")
+     fmt.Println("Input root data.")
+     var cities [100]City
+     file, err := os.Open("att48.tsp")
+     if err != nil {
+        fmt.Printf("Error: %s\n", err.Error())
+        return
+     }
      
+     lineNumber := 0
+     lineReader := bufio.NewReaderSize(file, 20)
+     for line, _, e := lineReader.ReadLine(); e==nil; line, _, e = lineReader.ReadLine() {
+         row := strings.Split(string(line), " ")
+         go func () {
+                  cities[lineNumber].num, _ = strconv.Atoi(row[0])
+                  cities[lineNumber].x, _   = strconv.ParseFloat(row[1], 64)
+                  cities[lineNumber].y, _   = strconv.ParseFloat(row[2], 64)
+                  if lineNumber<10 {
+                     fmt.Printf("%.3d: %3d %f %f\n", lineNumber, cities[lineNumber].num, cities[lineNumber].x, cities[lineNumber].y)
+                  } else {
+                       fmt.Printf("%.3d, ", lineNumber)
+                  }
+         } ()
+         lineNumber++
+     }
+     fmt.Println("...completed\ninput done.")
+
+    //fmt.Println("Do ACO")
+
+    //fmt.Println("Resutls")
+
+    fmt.Println("End Program.")
+   
 }
